ec134759ca5be1f26522139ec09ced376760ff00
attilaTorok <ansansans8@gmail.com>
2021-09-21 23:17:39 +0200

    Add conversionRate

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -9,6 +9,8 @@ contract FundMe {
     mapping(address => uint256) public addressToAmountFunded;
     
     function fund() public payable {
+        uint256 minimumUSD = 50 * 10 ** 18;
+        require(getConversionRate(msg.value) >= minimumUSD, "The sent value is not sufficient");
         addressToAmountFunded[msg.sender] += msg.value;
     }
     
@@ -21,6 +23,11 @@ contract FundMe {
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
         (,answer,,,) = priceFeed.latestRoundData();
         return uint256(answer);
+    }
+    
+    function getConversionRate(uint256 ethAmount) public view returns(uint256) {
+        uint256 ethPrice = getPrice();
+        return (ethPrice * ethAmount) * 1000000000000000000;
     }
     
 }
