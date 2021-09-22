d2f266cdde4896a9f05acf99cc74a35eda45ceee
attilaTorok <ansansans8@gmail.com>
2021-09-22 16:28:39 +0200

    Finish FundMe

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -2,32 +2,55 @@
 
 pragma solidity >=0.6.6 <0.9.0;
 
-import "@chainlink/contracts/src/v6.0/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe {
     
     mapping(address => uint256) public addressToAmountFunded;
+    address[] public founders;
+    address public owner;
+    
+    constructor() public {
+        owner = msg.sender;
+    }
     
     function fund() public payable {
         uint256 minimumUSD = 50 * 10 ** 18;
         require(getConversionRate(msg.value) >= minimumUSD, "The sent value is not sufficient");
         addressToAmountFunded[msg.sender] += msg.value;
+        founders.push(msg.sender);
     }
     
     function getVersion() public view returns(uint256) {
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
-        return priceFeed.getVersion();
+        return priceFeed.version();
     }
     
     function getPrice() public view returns(uint256) {
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
-        (,answer,,,) = priceFeed.latestRoundData();
+        (,int256 answer,,,) = priceFeed.latestRoundData();
         return uint256(answer);
     }
     
     function getConversionRate(uint256 ethAmount) public view returns(uint256) {
         uint256 ethPrice = getPrice();
         return (ethPrice * ethAmount) * 1000000000000000000;
+    }
+    
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+    
+    function withdraw() payable onlyOwner public {
+        msg.sender.transfer(address(this).balance);
+        
+        for (uint256 founderIndex = 0; founderIndex < founders.length; founderIndex++) {
+            address founder = founders[founderIndex];
+            addressToAmountFunded[founder] = 0;
+        }
+        
+        founders = new address[](0);
     }
     
 }
