6995d61527619b9b9a78ef4490eb651ca5768183
attilaTorok <ansansans8@gmail.com>
2021-09-23 22:41:04 +0200

    Deploy SimpleStorage to Rinkeby from web3

--- web3_py_simple_storage/deploy.py
+++ web3_py_simple_storage/deploy.py
@@ -1,6 +1,11 @@
 from solcx import compile_standard
 from solcx import install_solc
 import json
+from web3 import Web3
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 install_solc("0.6.0")
 
@@ -22,3 +27,39 @@ compiled_sol = compile_standard(
 
 with open("compiled_code.json", "w") as file:
     json.dump(compiled_sol, file)
+
+bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"][
+    "bytecode"
+]["object"]
+
+abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
+
+w3 = Web3(Web3.HTTPProvider(os.getenv("RINKEBY_ENDPOINT")))
+chain_id = int(os.getenv("RINKEBY_CHAIN_ID"))
+my_address = os.getenv("METAMASK_ACCOUNT")
+private_key = os.getenv("PRIVATE_KEY")
+
+SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
+nonce = w3.eth.getTransactionCount(my_address)
+transaction = SimpleStorage.constructor().buildTransaction(
+    {"chainId": chain_id, "from": my_address, "nonce": nonce}
+)
+signed_transaction = w3.eth.account.sign_transaction(
+    transaction, private_key=private_key
+)
+
+tx_hash = w3.eth.send_raw_transaction(signed_transaction.rawTransaction)
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+
+simple_storage = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
+
+print(simple_storage.functions.retrieveFavouriteNumber().call())
+store_transaction = simple_storage.functions.storeFavouriteNumber(15).buildTransaction(
+    {"chainId": chain_id, "from": my_address, "nonce": nonce + 1}
+)
+store_signed_transaction = w3.eth.account.sign_transaction(
+    store_transaction, private_key=private_key
+)
+tx_hash = w3.eth.send_raw_transaction(store_signed_transaction.rawTransaction)
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+print(simple_storage.functions.retrieveFavouriteNumber().call())
