61055e52e4f5c0d941edf307ca3c9ca7444f3e18
attilaTorok <ansansans8@gmail.com>
2021-09-24 22:09:27 +0200

    Brownie Fund Me

--- brownie_fund_me/contracts/FundMe.sol
+++ brownie_fund_me/contracts/FundMe.sol
@@ -1,56 +1,75 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.6.6 <0.9.0;
+pragma solidity ^0.6.6;
 
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
 
 contract FundMe {
-    
+    using SafeMathChainlink for uint256;
+
     mapping(address => uint256) public addressToAmountFunded;
     address[] public founders;
     address public owner;
-    
-    constructor() public {
+    AggregatorV3Interface public priceFeed;
+
+    constructor(address _priceFeed) public {
+        priceFeed = AggregatorV3Interface(_priceFeed);
         owner = msg.sender;
     }
-    
+
     function fund() public payable {
-        uint256 minimumUSD = 50 * 10 ** 18;
-        require(getConversionRate(msg.value) >= minimumUSD, "The sent value is not sufficient");
+        uint256 minimumUSD = 50 * 10**18;
+        require(
+            getConversionRate(msg.value) >= minimumUSD,
+            "The sent value is not sufficient"
+        );
         addressToAmountFunded[msg.sender] += msg.value;
         founders.push(msg.sender);
     }
-    
-    function getVersion() public view returns(uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+
+    function getVersion() public view returns (uint256) {
         return priceFeed.version();
     }
-    
-    function getPrice() public view returns(uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
-        (,int256 answer,,,) = priceFeed.latestRoundData();
+
+    function getPrice() public view returns (uint256) {
+        (, int256 answer, , , ) = priceFeed.latestRoundData();
         return uint256(answer);
     }
-    
-    function getConversionRate(uint256 ethAmount) public view returns(uint256) {
+
+    function getConversionRate(uint256 ethAmount)
+        public
+        view
+        returns (uint256)
+    {
         uint256 ethPrice = getPrice();
         return (ethPrice * ethAmount) * 1000000000000000000;
     }
-    
-    modifier onlyOwner {
+
+    function getEntranceFee() public view returns (uint256) {
+        uint256 mimimumUSD = 50 * 10**18;
+        uint256 price = getPrice();
+        uint256 precision = 1 * 10**18;
+        return (mimimumUSD * precision) / price;
+    }
+
+    modifier onlyOwner() {
         require(msg.sender == owner);
         _;
     }
-    
-    function withdraw() payable onlyOwner public {
+
+    function withdraw() public payable onlyOwner {
         msg.sender.transfer(address(this).balance);
-        
-        for (uint256 founderIndex = 0; founderIndex < founders.length; founderIndex++) {
+
+        for (
+            uint256 founderIndex = 0;
+            founderIndex < founders.length;
+            founderIndex++
+        ) {
             address founder = founders[founderIndex];
             addressToAmountFunded[founder] = 0;
         }
-        
+
         founders = new address[](0);
     }
-    
 }
