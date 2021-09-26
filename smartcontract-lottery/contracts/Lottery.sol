c7dbb6cba19adaa116b0105078b7b1a187214275
attilaTorok <ansansans8@gmail.com>
2021-09-26 23:38:10 +0200

    Start the lottery app

---
+++ smartcontract-lottery/contracts/Lottery.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.6;
+
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Lottery is OwnAble {
+    address payable[] public players;
+    uint256 public usdEntryFee;
+    AggregatorV3Interface internal ethUsdPriceFeed;
+    enum LOTTERY_STATE {
+        OPEN,
+        CLOSED,
+        CALCULATING_WINNER
+    }
+    LOTTERY_STATE public lottery_state;
+
+    constructor(address _priceFeedAddress) public {
+        usdEntryFee = 50 * 10**18;
+        ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
+        lottery_state = LOTTERY_STATE.CLOSED; // or 1
+    }
+
+    function enter() public payable {
+        // $50 minimum
+        require(
+            lottery_state == LOTTERY_STATE.OPEN,
+            "The lottery haven't been started or finished!"
+        );
+        require(msg.value >= getEntranceFee(), "Not enough ETH!");
+        players.push(msg.sender);
+    }
+
+    function getEntranceFee() public view returns (uint256) {
+        (, int256 price, , , ) = ethUsdPriceFeed.latestRoundData();
+        uint256 adjustedPrice = uint256(price) * 10**10;
+        // $50, $2,000 / ETH
+        uint256 costToEnter = (usdEntryFee * 10**18) / adjustedPrice;
+        return costToEnter;
+    }
+
+    function startLottery() public onlyOwner {
+        require(
+            lottery_state == LOTTERY_STATE.CLOSED,
+            "Can't start a new lottery yet!"
+        );
+        lottery_state = LOTTERY_STATE.OPEN;
+    }
+
+    function endLottery() public onlyOwner {}
+}
